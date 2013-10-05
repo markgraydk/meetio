34d20cf16509657a56f865eb6657db2bc044d162
Zaki Wasik <zaki@webconsu.lt>
2013-10-05 14:33:38 +0200

    Set up static files
    According to guide at
    https://devcenter.heroku.com/articles/django-assets
    
    Notice that you should do a: pip install dj-static

--- meetio/meetio/settings.py
+++ meetio/meetio/settings.py
@@ -156,3 +156,14 @@ LOGGING = {
         },
     }
 }
+
+# Static asset configuration
+import os
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_PATH, 'static'),
+)
+
